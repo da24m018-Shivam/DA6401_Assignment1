e9e7383e08c9f6397e45d8718f329ae0a01dbb47
Shivam Saxena <76241677+shivam-s16@users.noreply.github.com>
2025-03-01 22:55:28 +0530

    Backward Activation

--- activation.py
+++ activation.py
@@ -8,3 +8,15 @@ def tanh(Z):
 
 def relu(Z):
     return np.maximum(0, Z)
+
+def sigmoid_backward(dA, Z):
+    A = sigmoid(Z)
+    return dA * A * (1 - A)
+
+def tanh_backward(dA, Z):    
+    return dA * (1 - np.power(np.tanh(Z), 2))
+
+def relu_backward(dA, Z):
+    dZ = np.array(dA, copy=True)
+    dZ[Z <= 0] = 0
+    return dZ
